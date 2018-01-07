098a95e301c0d955ab9b3f65b38f09309f5c3040
kevinding0218 <kevinding0218@gmail.com>
2018-01-07 03:18:57 -0500

    allow cors only for api host port

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,7 @@ namespace FamilyAssistant {
             corsBuilder.AllowAnyHeader ();
             corsBuilder.AllowAnyMethod ();
             corsBuilder.AllowAnyOrigin (); // For anyone access.
-            //corsBuilder.WithOrigins("http://localhost:56573"); // for a specific url. Don't add a forward slash on the end!
+            corsBuilder.WithOrigins("http://localhost:62657"); // for a specific url. Don't add a forward slash on the end!
             corsBuilder.AllowCredentials ();
 
             services.AddCors (options => {
